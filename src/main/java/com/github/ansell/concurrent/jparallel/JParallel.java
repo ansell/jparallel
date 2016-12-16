c738a1cc6aa67cab4bb63a2a81dd52b7250427a0
Peter Ansell <p_ansell@yahoo.com>
2016-12-16 11:53:50 +1100

    Add stub utility
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ src/main/java/com/github/ansell/concurrent/jparallel/JParallel.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016, Peter Ansell
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.ansell.concurrent.jparallel;
+
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+/**
+ * Command line for testing jparallel operations
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public final class JParallel {
+
+	public static void main(String... args) throws Exception {
+		final OptionParser parser = new OptionParser();
+
+		final OptionSpec<Void> help = parser.accepts("help").forHelp();
+		final OptionSpec<Boolean> debug = parser.accepts("debug").withRequiredArg().ofType(Boolean.class)
+				.defaultsTo(Boolean.FALSE).describedAs("Set to true to debug.");
+
+		OptionSet options = null;
+
+		try {
+			options = parser.parse(args);
+		} catch (final OptionException e) {
+			System.out.println(e.getMessage());
+			parser.printHelpOn(System.out);
+			throw e;
+		}
+
+		if (options.has(help)) {
+			parser.printHelpOn(System.out);
+			return;
+		}
+
+	}
+
+}
